946efcdb08c19a085c2b5c6e7b763fc855ee3709
Logan McAnsh <logan@mcan.sh>
2019-04-05 18:42:03 -0400

    update HUE_BRIDGE_API ip address
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 const withTypescript = require('@zeit/next-typescript');
 
 const HUE_BRIDGE_USERNAME = 'gT3baNOF-fXFvjZBZBTQPy7-wlygSlhdtjh-IgQV';
-const HUE_BRIDGE_URL = `http://10.0.1.144/api`;
-const HUE_BRIDGE_API = `http://10.0.1.144/api/${HUE_BRIDGE_USERNAME}`;
+const HUE_BRIDGE_API = `http://10.0.1.15/api/${HUE_BRIDGE_USERNAME}`;
 
 const nextConfig = {
   target: 'serverless',
@@ -10,7 +9,6 @@ const nextConfig = {
   env: {
     VERSION: require('./package.json').version,
     HUE_BRIDGE_USERNAME,
-    HUE_BRIDGE_URL,
     HUE_BRIDGE_API,
   },
 };
